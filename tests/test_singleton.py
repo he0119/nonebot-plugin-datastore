31f89fd54ec46c75f9568dd21c827a3fd5f9c613
hemengyang <hmy0119@gmail.com>
2022-01-24 20:19:41 +0800

    test: 完善测试

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -15,8 +15,26 @@ async def test_singleton(app: App):
 
 
 @pytest.mark.asyncio
+async def test_singleton_keyword(app: App):
+    """测试单例
+
+    一个使用位置形参，一个使用关键字形参
+    """
+    from nonebot_plugin_datastore import PluginData
+
+    data1 = PluginData("test")
+    data2 = PluginData(name="test")
+    assert data1 is data2
+    data1.config.set("test", 1)
+    assert data2.config.get("test") == 1
+
+
+@pytest.mark.asyncio
 async def test_singleton_different(app: App):
-    """测试单例"""
+    """测试单例
+
+    不同名称的情况
+    """
     from nonebot_plugin_datastore import PluginData
 
     data1 = PluginData("test")
