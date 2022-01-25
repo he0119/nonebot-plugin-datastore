24101bcda2902d6cb7ab1f9d4beeee1c2194fc08
hemengyang <hmy0119@gmail.com>
2022-01-25 21:16:38 +0800

    test: 完善 PluginData 的测试

--- tests/test_open.py
+++ tests/test_open.py
@@ -3,36 +3,74 @@ from nonebug import App
 
 
 @pytest.mark.asyncio
-async def test_open_data_file(app: App):
-    """测试打开数据文件"""
+async def test_exists(app: App):
+    """测试文件是否存在"""
     from nonebot_plugin_datastore import PluginData
     from nonebot_plugin_datastore.config import plugin_config
 
-    test_file = plugin_config.datastore_data_dir / "test" / "test.txt"
+    data = PluginData("test")
+
+    test_file = plugin_config.datastore_data_dir / "test" / "data"
+    assert test_file.exists() is False
+    assert data.exists("data") is False
+    test_file.touch()
+    assert test_file.exists() is True
+    assert data.exists("data") is True
+
+    test_file = plugin_config.datastore_cache_dir / "test" / "cache"
+    assert test_file.exists() is False
+    assert data.exists("cache", cache=True) is False
+    test_file.touch()
+    assert test_file.exists() is True
+    assert data.exists("cache", cache=True) is True
+
+
+@pytest.mark.asyncio
+async def test_open_file(app: App):
+    """测试打开文件"""
+    from nonebot_plugin_datastore import PluginData
+    from nonebot_plugin_datastore.config import plugin_config
 
     data = PluginData("test")
 
+    test_file = plugin_config.datastore_data_dir / "test" / "test.txt"
     assert test_file.exists() is False
     test_file.write_text("test")
 
-    assert data.exists("test.txt") is True
     with data.open("test.txt", "r") as f:
+        assert f.read() == "test"
+
+    test_file = plugin_config.datastore_cache_dir / "test" / "test.txt"
+    assert test_file.exists() is False
+    test_file.write_text("test")
+
+    with data.open("test.txt", "r", cache=True) as f:
         assert f.read() == "test"
 
 
 @pytest.mark.asyncio
-async def test_open_cache_file(app: App):
-    """测试打开缓存文件"""
+async def test_dump_load_data(app: App):
+    """测试 dump 和 load 数据"""
     from nonebot_plugin_datastore import PluginData
-    from nonebot_plugin_datastore.config import plugin_config
-
-    test_file = plugin_config.datastore_cache_dir / "test" / "test.txt"
 
     data = PluginData("test")
 
-    assert test_file.exists() is False
-    test_file.write_text("test")
+    test = {"test": 1}
+    data.dump_pkl(test, "test.pkl")
+    assert data.exists("test.pkl") is True
+    assert data.load_pkl("test.pkl") == test
 
-    assert data.exists("test.txt", cache=True) is True
-    with data.open("test.txt", "r", cache=True) as f:
-        assert f.read() == "test"
+    test = {"test": 2}
+    data.dump_pkl(test, "test.pkl", cache=True)
+    assert data.exists("test.pkl", cache=True) is True
+    assert data.load_pkl("test.pkl", cache=True) == test
+
+    test = {"test": 3}
+    data.dump_json(test, "test.json")
+    assert data.exists("test.json") is True
+    assert data.load_json("test.json") == test
+
+    test = {"test": 4}
+    data.dump_json(test, "test.json", cache=True)
+    assert data.exists("test.json", cache=True) is True
+    assert data.load_json("test.json", cache=True) == test
