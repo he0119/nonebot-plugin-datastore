91172771b218bd9cbae30c59a878426f9e61250f
hemengyang <hmy0119@gmail.com>
2022-02-08 00:24:50 +0800

    test: 完善数据库相关的测试

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import nonebot
 import pytest
 from nonebug import App
@@ -24,3 +26,45 @@ async def test_db(app: App):
         statement = select(Example)
         example: Example = (await session.exec(statement)).first()  # type: ignore
         assert example.message == "test"
+
+
+@pytest.mark.asyncio
+async def test_disable_db(nonebug_init: None, tmp_path: Path):
+    """测试禁用数据库"""
+    import nonebot
+
+    config = nonebot.get_driver().config
+    # 插件数据目录
+    config.datastore_cache_dir = tmp_path / "cache"
+    config.datastore_config_dir = tmp_path / "config"
+    config.datastore_data_dir = tmp_path / "data"
+
+    # 禁用数据库
+    config.datastore_enable_database = False
+
+    # 加载插件
+    nonebot.load_plugin("nonebot_plugin_datastore")
+
+    from nonebot_plugin_datastore import create_session
+
+    with pytest.raises(ValueError) as e:
+        async with create_session() as session:
+            pass
+
+    assert str(e.value) == "数据库未启用"
+
+
+@pytest.mark.asyncio
+async def test_default_db_url(nonebug_init: None):
+    """测试默认数据库地址"""
+    import nonebot
+
+    # 加载插件
+    nonebot.load_plugin("nonebot_plugin_datastore")
+
+    from nonebot_plugin_datastore.config import BASE_DATA_DIR, plugin_config
+
+    assert (
+        plugin_config.datastore_database_url
+        == f"sqlite+aiosqlite:///{BASE_DATA_DIR / 'data.db'}"
+    )
