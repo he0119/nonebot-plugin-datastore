6e6a001ff52494dcef49dcd0656f9f5373b7122b
hemengyang <hmy0119@gmail.com>
2023-01-15 00:30:31 +0800

    添加一些测试

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,6 @@
+import asyncio
+from pathlib import Path
+
 from click.testing import CliRunner
 from nonebug import App
 
@@ -28,3 +31,47 @@ def test_cli_help(app: App):
     assert result.exit_code == 0
     assert "--name" in result.output
     assert "[REVISION]" in result.output
+
+
+def test_revision(app: App, tmp_path: Path):
+    from nonebot import require
+
+    from nonebot_plugin_datastore.db import init_db
+    from nonebot_plugin_datastore.script.cli import cli
+
+    require("tests.example2")
+    asyncio.run(init_db())
+
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=tmp_path) as td:
+        result = runner.invoke(cli, ["revision", "--autogenerate", "--message", "test"])
+        assert result.exit_code == 0
+        assert "" in result.output
+
+
+def test_upgrade(app: App):
+    from nonebot import require
+
+    from nonebot_plugin_datastore.script.cli import cli
+
+    require("tests.example")
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ["upgrade"])
+    assert result.exit_code == 0
+    assert "" in result.output
+
+
+def test_downgrade(app: App):
+    from nonebot import require
+
+    from nonebot_plugin_datastore.db import init_db
+    from nonebot_plugin_datastore.script.cli import cli
+
+    require("tests.example")
+    asyncio.run(init_db())
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ["downgrade"])
+    assert result.exit_code == 0
+    assert "" in result.output
