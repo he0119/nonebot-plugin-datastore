9b02708175406807676811d6ac9fa48560e11c4f
uy/sun <hmy0119@gmail.com>
2023-02-14 14:53:19 +0800

    fix: 修复运行中删除目录导致报错的问题 (#86)
    * fix: 修复运行中删除目录导致报错的问题
    
    * 添加测试

--- nonebot_plugin_datastore/config.py
+++ nonebot_plugin_datastore/config.py
@@ -8,36 +8,45 @@ from pydantic import BaseModel, Extra, root_validator
 require("nonebot_plugin_localstore")
 from nonebot_plugin_localstore import get_cache_dir, get_config_dir, get_data_dir
 
-# 默认目录
-BASE_CACHE_DIR: Path = Path(get_cache_dir(""))
-BASE_CONFIG_DIR: Path = Path(get_config_dir(""))
-BASE_DATA_DIR: Path = Path(get_data_dir(""))
-
 
 class Config(BaseModel, extra=Extra.ignore):
-    datastore_cache_dir: Path = BASE_CACHE_DIR
-    datastore_config_dir: Path = BASE_CONFIG_DIR
-    datastore_data_dir: Path = BASE_DATA_DIR
-    datastore_enable_database: bool = True
+    datastore_cache_dir: Path
+    datastore_config_dir: Path
+    datastore_data_dir: Path
     datastore_database_url: str
     """数据库连接字符串
 
     默认使用 SQLite
     """
+    datastore_enable_database: bool = True
     datastore_database_echo: bool = False
 
     @root_validator(pre=True, allow_reuse=True)
-    def set_database_url(cls, values: Dict):
-        database_url = values.get("datastore_database_url")
-        data_dir = values.get("datastore_data_dir")
-        if database_url is None:
-            if data_dir is None:
-                data_dir = BASE_DATA_DIR
-            else:
-                data_dir = Path(data_dir)
+    def set_defaults(cls, values: Dict):
+        """设置默认值"""
+        # 设置默认目录
+        # 仅在未设置时调用 get_*_dir 函数，因为这些函数会自动创建目录
+        values["datastore_cache_dir"] = (
+            Path(cache_dir)
+            if (cache_dir := values.get("datastore_cache_dir"))
+            else Path(get_cache_dir(""))
+        )
+        values["datastore_config_dir"] = (
+            Path(config_dir)
+            if (config_dir := values.get("datastore_config_dir"))
+            else Path(get_config_dir(""))
+        )
+        values["datastore_data_dir"] = (
+            Path(data_dir)
+            if (data_dir := values.get("datastore_data_dir"))
+            else Path(get_data_dir(""))
+        )
+
+        # 设置默认数据库连接字符串
+        if not values.get("datastore_database_url"):
             values[
                 "datastore_database_url"
-            ] = f"sqlite+aiosqlite:///{data_dir / 'data.db'}"
+            ] = f"sqlite+aiosqlite:///{values['datastore_data_dir'] / 'data.db'}"
         return values
 
 
